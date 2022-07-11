f9d118a4c1af65b84b7983c64baf793f4800b8b1
kapilkumarpass <kapilkmr678@gmail.com>
2022-07-11 23:05:23 +0530

    basic frontend added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import Button from '@mui/material/Button';
+import Signin from './components/Signin.js'
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Signup from './components/Signup.js'
+import Home from './components/Home.js'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+    <Routes>
+      <Route 
+        path="/" 
+        element={<Signin />}
+      />
+      <Route
+        path="/signup"
+        element={<Signup />}
+      />
+      <Route
+        path="/home"
+        element={<Home />}
+      />
+     
+  </Routes>
+  </Router>
   );
 }
 
